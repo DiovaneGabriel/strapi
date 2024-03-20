c740d74b7aece163fe8a05e3f2ed53655030dab3
Diovane <diovane.gabriel@gmail.com>
2024-03-20 15:16:52 -0300

    s3

--- app/config/middlewares.ts
+++ app/config/middlewares.ts
@@ -1,7 +1,34 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  //'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
