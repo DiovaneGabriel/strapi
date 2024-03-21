a8dee5d3f858ac804c38748df57dda70c15fc080
Diovane <diovane.gabriel@gmail.com>
2024-03-21 16:20:38 -0300

    restart

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 NODE_IMAGE = strapi-node-app:1.0
+NODE_CONTAINER = node
 
 up:
 	clear && \
@@ -8,7 +9,28 @@ down:
 	clear && \
 	docker compose down
 
+bash:
+	docker exec -it ${NODE_CONTAINER} /bin/sh
+
+build-image:
+	docker build -f .docker/node/Dockerfile -t ${NODE_IMAGE} .
+
+update:
+	docker exec -it ${NODE_CONTAINER} /bin/sh -c "npm i"
+
+network:
+	docker network create strapi
+
 install:
-	clear && \
-	docker build -f .docker/node/Dockerfile -t ${NODE_IMAGE} . && \
-	docker run -it -v ./app:/app ${NODE_IMAGE} /bin/sh -c "npm update"
+	make network && \
+	make build-image && \
+	docker run -it -v ./app:/app ${NODE_IMAGE} /bin/sh -c "npx create-strapi-app@latest . && npm i" && \
+	sudo chmod -R 777 . && \
+	mv ./app/* . && \
+	mv ./app/.editorconfig . && \
+	mv ./app/.env . && \
+	mv ./app/.env.example . && \
+	mv ./app/.gitignore . && \
+	rm -rf ./app && \
+	docker container prune -f && \
+	docker compose up -d
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -590,53 +590,6 @@ export interface PluginContentReleasesReleaseAction
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
-  info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
-    description: '';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<
-        {
-          min: 1;
-          max: 50;
-        },
-        number
-      >;
-    code: Attribute.String & Attribute.Unique;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 export interface PluginUsersPermissionsPermission
   extends Schema.CollectionType {
   collectionName: 'up_permissions';
@@ -788,69 +741,46 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface ApiFilmFilm extends Schema.CollectionType {
-  collectionName: 'films';
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
   info: {
-    singularName: 'film';
-    pluralName: 'films';
-    displayName: 'film';
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
     description: '';
   };
   options: {
-    draftAndPublish: true;
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
   };
   attributes: {
-    title: Attribute.String;
-    released: Attribute.Date;
-    director: Attribute.String;
-    plot: Attribute.Text;
-    slug: Attribute.String;
-    reviews: Attribute.Relation<
-      'api::film.film',
-      'oneToMany',
-      'api::review.review'
-    >;
-    image: Attribute.Media;
+    name: Attribute.String &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+          max: 50;
+        },
+        number
+      >;
+    code: Attribute.String & Attribute.Unique;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<'api::film.film', 'oneToOne', 'admin::user'> &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<'api::film.film', 'oneToOne', 'admin::user'> &
-      Attribute.Private;
-  };
-}
-
-export interface ApiReviewReview extends Schema.CollectionType {
-  collectionName: 'reviews';
-  info: {
-    singularName: 'review';
-    pluralName: 'reviews';
-    displayName: 'Review';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    review: Attribute.Text;
-    reviewer: Attribute.String;
-    film: Attribute.Relation<
-      'api::review.review',
-      'manyToOne',
-      'api::film.film'
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
-      'api::review.review',
+      'plugin::i18n.locale',
       'oneToOne',
       'admin::user'
     > &
       Attribute.Private;
     updatedBy: Attribute.Relation<
-      'api::review.review',
+      'plugin::i18n.locale',
       'oneToOne',
       'admin::user'
     > &
@@ -872,12 +802,10 @@ declare module '@strapi/types' {
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
-      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'api::film.film': ApiFilmFilm;
-      'api::review.review': ApiReviewReview;
+      'plugin::i18n.locale': PluginI18NLocale;
     }
   }
 }
